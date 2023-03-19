8626974245f507145874cf56c207bb653910c789
albim72 <marcin.albiniak72@gmail.com>
2023-03-19 13:45:47 +0100

    Create prod1.py

---
+++ BUILDER_PATTERN/prod1.py
@@ -0,0 +1,10 @@
+from typing import Any
+class Product1:
+    def __init__(self)->None:
+        self.parts = []
+
+    def add(self,part:Any)->None:
+        self.parts.append(part)
+
+    def list_parts(self):
+        print(f'Części produktu: {", ".join(self.parts)}',end="")
